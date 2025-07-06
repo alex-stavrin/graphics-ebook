2ad03f9cf1a22c8efa09f45fc74eac0db57ba6d4
Alekossta <alekosstavrinoudakis@gmail.com>
2025-07-06 12:18:39 +0300

    finished apokopi

--- src/eisagogi/apokopi.jsx
+++ src/eisagogi/apokopi.jsx
@@ -6,6 +6,9 @@ import csEx2 from '../assets/eisagogi/cs-clipping/ex2.png';
 import numbered from '../assets/eisagogi/lb-clip/numbered.png';
 import directions from '../assets/eisagogi/lb-clip/directions.png';
 import example from '../assets/eisagogi/lb-clip/example.png';
+import exampleSolution from '../assets/eisagogi/lb-clip/exampleSolution.png';
+import polygonClip from '../assets/eisagogi/polygon-clip.png';
+import polygonStages from '../assets/eisagogi/polygon-stages.png';
 import { Stage, Layer, Line, Text, Circle } from 'react-konva';
 import { useState } from "react";
 
@@ -158,5 +161,70 @@ export default function Apokopi()
         <p>Αν <InlineMath math="t_{in} \le t_{out}"/> τότε τα βάζουμε στην ευθεία για να υπολογίσουμε το αποκόμενο τμήμα</p>
         <h5>Παράδειγμα LB</h5>
         <img src={example} width={400} height={400} alt="LB example image"/>
+        <p><InlineMath math="Δx = 3 - 0.5 = 2.5"/></p>
+        <p><InlineMath math="Δy = 3 - 0.5 = 2.5"/></p>
+        <p>Άρα τα <InlineMath math="p_i"/> προκύπτουν</p>
+        <ol className="list-decimal ml-5">
+            <li>
+                <InlineMath math="p_1=-Δx=-2.5"/>
+            </li>
+            <li>
+                <InlineMath math="p_2=Δx=2.5"/>
+            </li>
+            <li>
+                <InlineMath math="p_3=-Δy=-2.5"/>
+            </li>
+            <li>
+                <InlineMath math="p_4=Δy=-2.5"/>
+            </li>       
+        </ol>
+        <p>όρια οθόνης:</p>
+        <p><InlineMath math="x_{min}=1, x_{max}=4"/></p>
+        <p><InlineMath math="y_{min}=1, y_{max}=4"/></p>
+        <p>Υπολογίζουμε τα <InlineMath math="q_i"/> με βάση τους τύπους:</p>
+        <ol className="list-decimal ml-5">
+            <li>
+                <InlineMath math="q_1=x_1 - x_{min}= 0.5 - 1 = -0.5"/>
+            </li>
+            <li>
+                <InlineMath math="q_2 = x_{max} - x_1 = 4 - 0.5 = 3.5"/>
+            </li>
+            <li>
+                <InlineMath math="q_3=y_1-y_{min}=0.5 - 1 = -0.5"/>
+            </li>
+            <li>
+                <InlineMath math="q_4=y_{max}-y_1= 4 - 0.5 = 3.5"/>
+            </li>
+        </ol>
+        <p>Τώρα θα υπολογίσουμε το <InlineMath math="t_{in}"/>. Αρχικά απο τον τύπο θέλουμε <InlineMath math="p_i < 0"/>. Άρα θα πάρουμε τα 1 και 3.</p>
+        <p><InlineMath math="\frac{q_1}{p_1}=\frac{-0.5}{-2.5}=0.2"/></p>
+        <p><InlineMath math="\frac{q_3}{p_3}=\frac{-0.5}{-2.5}=0.2"/></p>
+        <p><InlineMath math="t_{in}=max(0.2,0.2,0.0)=0.2"/></p>
+        <p>Στην συνέχεια υπολογιζούμε και το <InlineMath math="t_{out}"/>. Εδώ θέλουμε <InlineMath math="p_i > 0"/>. Άρα παίρνουμε τα 2 και 4</p>
+        <p><InlineMath math="\frac{q_2}{p_2}=\frac{3.5}{2.5}=1.4"/></p>
+        <p><InlineMath math="\frac{q_4}{p_4}=\frac{3.5}{2.5}=1.4"/></p>
+        <p><InlineMath math="t_{out}=min(1.4,1.4,1)=1"/></p>
+        <p>Αφού <InlineMath math="t_{in} < t_{out}"/> τα βάζουμε στην εξίσωση της ευθείας και βρίσκουμε το τελικο ευθύγραμμο τμήμα που θα αποτελείται
+         απο τα σημεία <InlineMath math="p_3(x_3,y_3), p_4(x_4,y_4)"/></p>
+        <p><InlineMath math="x_3=x_1+t_{in}Δx=0.5+0.2 \cdot 2.5=1"/></p>
+        <p><InlineMath math="y_3=y_1+t_{in}Δy=0.5+0.2 \cdot 2.5=1"/></p>
+        <p><InlineMath math="x_4=x_1+t_{out}Δx=0.5+1 \cdot 2.5=3"/></p>
+        <p><InlineMath math="y_4=y_1+t_{out}Δy=0.5+1 \cdot 2.5=3"/></p>
+        <p>Άρα <InlineMath math="p_3 = (1,1), p_4=(3,3)"/> και τελική ευθεία</p>
+        <img src={exampleSolution} width={400} height={400} alt="LB example solution image"/>
+        <h3>Αποκοπή Πολυγώνου</h3>
+        <img src={polygonClip} width={400} height={400} alt="Polygon clip image"/>
+        <h4>Αλγόριθμος Sutherland - Hodgman (SH)</h4>
+        <p>Το αντικείμενο που κάνει την αποκοπή και το αντικείμενο που δέχεται την αποκοπή είναι πολύγωνα.</p>
+        <p>Αποτελείται απο m στάδια οπου m είναι οι ακμές του πολυγώνου που κάνει την αποκοπή. Αφου συνήθως το αντικείμενο
+            που κάνει την αποκοπή είναι ένα ορθογώνιο παραλληλόγραμμο που αναπαριστά την οθόνη, m = 4
+        </p>
+        <p>Σε αυτήν την τεχνική πάλι αριθμίζουμε τις πλευρές την οθόνης και σε κάθε στάδιο κόβουμε το αποκοπτόμενο πολύγωνο ως προς αυτήν την πλευρά
+            της οθόνης
+        </p>
+        <p>Σε κάθε στάδιο i κόβουμε προς την πλευρά της οθόνης i. i απο 0 εως 3</p>
+        <img src={polygonStages} width={400} height={400} alt="Polygon clip stages"/>
+        <p>Η πολυπλοκότητα του αλγοριθμου είναι <InlineMath math="O(mn)"/>, n = πλήθος κορυφών αποκοπτούμενου πολυγώνου και m = πλήθος κορυφών
+        πολυγώνου αποκοπής (οθόνης)</p>
     </div>
 }
