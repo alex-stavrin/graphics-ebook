6b5336b9d8dcd300b25d450ebcec9e7ac710fd3a
Alekos Stavrinoudakis <alekosstavrinoudakis@gmail.com>
2025-07-10 08:29:44 +0300

    doing complicated

--- src/metasxhmatismoi/metasxhmatismoi-eisagogi.jsx
+++ src/metasxhmatismoi/metasxhmatismoi-eisagogi.jsx
@@ -281,5 +281,15 @@ export default function MetasxhmatismoiEisagogi()
                 </Layer>
             </Stage>
         </div>
+        <h2>Σύνθετοι Μετασχηματισμοί</h2>
+        <p>Έστω οτι θέλουμε να περιστρέψουμε ένα αντικείμενο κατα 45 μοίρες και να διπλασιάσουμε το μέγεθος του. Υπάρχουν δύο τρόποι:</p>
+        <ol className="list-decimal ml-5">
+            <li>
+                Να εφαρμόσουμε τους μετασχηματισμούς έναν έναν στο σημείο. <InlineMath math="SCALE \cdot (ROTATION \cdot point)"/> 
+            </li>
+            <li>
+                Να φτιάξουμε έναν νέο πίνακα που να συνδιάζει όλους του μετασχηματισμούς. <InlineMath math="(SCALE \cdot ROTATION) \cdot POINT"/>
+            </li>
+        </ol>
     </div>
 }
