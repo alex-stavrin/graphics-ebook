4462d62ef2623114281bf878d187dd1ea09ba512
Alekos Stavrinoudakis <alekosstavrinoudakis@gmail.com>
2025-08-15 19:17:45 +0300

    added warning

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,6 +5,7 @@ export default function Home()
         <p>Αυτό το "ebook" αποτελεί ένα βοήθημα για την προετοιμασία στο μάθημα Γραφικά 1 του Dit UOA. Το υλικό είναι βασισμένο στην ύλη
             και διαφανείες του καθηγητή Θεοχάρη Θεοχάρη.
         </p>
+        <p>Υπάρχει πιθάνοτητα να υπάρχουν λάθη. Άμα βρείτε κάτι στείλτε μου στο discord. Username: alex_stavrin</p>
         <p><a href="https://www.alexstavrin.com/" target="_blank">Made by Alex Stavrin</a></p>
         <p><a href="https://github.com/alex-stavrin/graphics-ebook" target="_blank">Κάνε commit στο ebook</a></p>
     </div>
