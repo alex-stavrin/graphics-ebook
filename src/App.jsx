ac929c0c71a3ac6f0d2b9feab2442dbd60775cdb
alex_stavrin <alekosstavrinoudakis@gmail.com>
2025-09-06 18:07:38 +0300

    working on iounios 2023

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import Skies from "./realismos/skies";
 import Home from "./Home";
 import Xeimerino2025 from "./themata/xeimerino2025";
 import Septembrios2024 from "./themata/septembrios2024";
+import Iounios2023 from "./themata/iounios2023";
 
 function App() {
 
@@ -55,6 +56,7 @@ function App() {
         <h3 className="ml-3">5. Παλιά Θέματα</h3>
         <h4 className="ml-6"><Link to={"/θεματα/χειμερινο2025"}>Χειμερινό Εξάμηνο 2025</Link></h4>
         <h4 className="ml-6"><Link to={"/θεματα/σεπτεμβριος2024"}>Σεπτέμβριος 2024</Link></h4>
+        <h4 className="ml-6"><Link to={"/θεματα/ιουνιος2023"}>Ιούνιος 2023</Link></h4>
 
 
       </div>
@@ -83,6 +85,7 @@ function App() {
 
           <Route path="/θεματα/χειμερινο2025" element={<Xeimerino2025/>}/>
           <Route path="/θεματα/σεπτεμβριος2024" element={<Septembrios2024/>}/>
+          <Route path="/θεματα/ιουνιος2023" element={<Iounios2023/>}/>
         </Routes>
       </div>
     </div>
